0e7e982a5a1e3eef39ab66e0696eb95715435cab
Akimasa Tateba <akimasa@gmail.com>
2018-10-01 23:16:37 +0900

    install eslint

--- index.js
+++ index.js
@@ -1,67 +1,66 @@
 var cropdetail;
 var PICTWIDTH = 30;
 var PICTHEIGHT = 40;
-function setsize(){
-    var height = $("#height").val();
-    var width = $("#width").val();
-    PICTHEIGHT = parseFloat(height);
-    PICTWIDTH = parseFloat(width);
+function setsize() {
+  var height = $("#height").val();
+  var width = $("#width").val();
+  PICTHEIGHT = parseFloat(height);
+  PICTWIDTH = parseFloat(width);
 }
-function draw(){
-    
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.height = 2102;//89mm
-    canvas.width = 3000;//127mm
-    var hei = canvas.height / 89 * PICTHEIGHT;
-    var wid = canvas.width / 127 * PICTWIDTH;
-    var buf = canvas.width / 127 * 2;// 5mm buffer
-    var x = buf;
-    var y = buf;
-    ctx.fillStyle = "rgb(255,255,255)";
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-    ctx.fillStyle = "rgb(0,0,0)";
-    while (y < canvas.height) {
-        while (x < canvas.width) {
-            ctx.drawImage(cropper.getCroppedCanvas(),x, y, wid, hei);
-            ctx.beginPath();
-            ctx.moveTo(x,y);
-            ctx.lineTo(x+wid,y);
-            ctx.lineTo(x+wid,y+hei);
-            ctx.lineTo(x,y+hei);
-            ctx.lineTo(x,y);
-            ctx.stroke();
-            x += wid + buf;
-        }
-        y += hei + buf;
-        x = buf;
+function draw() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.height = 2102; //89mm
+  canvas.width = 3000; //127mm
+  var hei = (canvas.height / 89) * PICTHEIGHT;
+  var wid = (canvas.width / 127) * PICTWIDTH;
+  var buf = (canvas.width / 127) * 2; // 5mm buffer
+  var x = buf;
+  var y = buf;
+  ctx.fillStyle = "rgb(255,255,255)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgb(0,0,0)";
+  while (y < canvas.height) {
+    while (x < canvas.width) {
+      ctx.drawImage(cropper.getCroppedCanvas(), x, y, wid, hei);
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x + wid, y);
+      ctx.lineTo(x + wid, y + hei);
+      ctx.lineTo(x, y + hei);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      x += wid + buf;
     }
-    var outputjpg = canvas.toDataURL("image/jpeg");
-    $("#output").attr("src", outputjpg);
-    $("#outlink").attr("href", outputjpg);
+    y += hei + buf;
+    x = buf;
+  }
+  var outputjpg = canvas.toDataURL("image/jpeg");
+  $("#output").attr("src", outputjpg);
+  $("#outlink").attr("href", outputjpg);
 }
 function crop() {
-    draw();
+  draw();
 }
-function fileselect(e){
-    var file = e.target.files[0];
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function(){
-        var dataurl = reader.result;
-        document.getElementById("image").src = dataurl;
-        var $image = $('#image');
+function fileselect(e) {
+  var file = e.target.files[0];
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = function() {
+    var dataurl = reader.result;
+    document.getElementById("image").src = dataurl;
+    var $image = $("#image");
 
-        $image.cropper({
-        aspectRatio: PICTWIDTH / PICTHEIGHT,
-        autoCropArea: 1,
-        crop: function(event) {
-            cropdetail = event.detail;
-        }
-        });
+    $image.cropper({
+      aspectRatio: PICTWIDTH / PICTHEIGHT,
+      autoCropArea: 1,
+      crop: function(event) {
+        cropdetail = event.detail;
+      }
+    });
 
-        // Get the Cropper.js instance after initialized
-        cropper = $image.data('cropper');
-    }
+    // Get the Cropper.js instance after initialized
+    cropper = $image.data("cropper");
+  };
 }
 document.getElementById("file").addEventListener("change", fileselect);
